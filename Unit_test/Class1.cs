b5c5ed3e87ab4f4c2087a1fc3ff2e19a39d759da
David Jones <nikitatopol1501919@mail.ru>
2022-05-27 03:44:37 +0500

    Add project files.

---
+++ Unit_test/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_test
+{
+    public class Class1
+    {
+    public static bool ValidatePassword(string password)
+    {
+      if (password.Length < 8 || password.Length > 20)
+        return false;
+      if (!password.Any(char.IsLower))
+        return false;
+      if (!password.Any(char.IsUpper))
+        return false;
+      if (!password.Any(char.IsDigit))
+        return false;
+      if (password.Intersect("#$%^&_").Count() == 0)
+        return false;
+
+      return true;
+    }
+    }
+}
---
+++ Unit_testTests/Class1Tests.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unit_test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_test.Tests
+{
+  [TestClass()]
+  public class Class1Tests
+  {
+    [TestMethod()]
+    public void Check_10Symbols_ReturnTrue()
+    {
+      //arrange
+      string password = "ASqw12$$";
+      bool expected = true;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_4Symbols_ReturnFalse()
+    {
+      //arrange
+      string password = "Aq1$";
+      bool expected = false;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_30Symbols_ReturnFalse()
+    {
+      //arrange
+      string password = "ASDqwe123$ASDqwe123$ASDqwe123$";
+      bool expected = false;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_PasswordWithDigit_ReturnTrue()
+    {
+      //arrange
+      string password = "ASDqwe1$";
+      bool expected = true;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_PasswordWithoutDigit_ReturnFalse()
+    {
+      //arrange
+      string password = "ASDqweASD$";
+      bool expected = false;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_PasswordWithSpecSymbol_ReturnTrue()
+    {
+      //arrange
+      string password = "Aqwe123$";
+      bool expected = true;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_PasswordWithoutSpecSymbol_ReturnFalse()
+    {
+      //arrange
+      string password = "ASDqwe123";
+      bool expected = false;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_PasswordWithCapsSymbol_ReturnTrue()
+    {
+      //arrange
+      string password = "Aqwe123$";
+      bool expected = true;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_PasswordWithoutCapsSymbol_ReturnFalse()
+    {
+      //arrange
+      string password = "asdqwe123$";
+      bool expected = false;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_PasswordWithLowerSymbol_ReturnTrue()
+    {
+      //arrange
+      string password = "ASDq123$";
+      bool expected = true;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod()]
+    public void Check_PasswordWithoutLowerSymbol_ReturnFalse()
+    {
+      //arrange
+      string password = "ASDQWE123$";
+      bool expected = false;
+
+      //act
+      bool actual = Class1.ValidatePassword(password);
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+  }
+}
